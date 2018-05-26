2af5a1dd9e74cff78bcb31d6d14e3cb083bb85c9
JoMingyu <city7310@naver.com>
2018-05-27 01:10:03 +0900

    DONE: SchoolAPI 클래스, Region, Type Object

---
+++ index.js
@@ -0,0 +1,31 @@
+class SchoolAPI {
+}
+
+SchoolAPI.Region = {
+    SEOUL: 'stu.sen.go.kr',
+    BUSAN: 'stu.pen.go.kr',
+    DAEGU: 'stu.dge.go.kr',
+    INCHEON: 'stu.ice.go.kr',
+    GWANGJU: 'stu.gen.go.kr',
+    DAEJEON: 'stu.dje.go.kr',
+    ULSAN: 'stu.use.go.kr',
+    SEJONG: 'stu.sje.go.kr',
+    GYEONGGI: 'stu.cbe.go.kr',
+    KANGWON: 'stu.kwe.go.kr',
+    CHUNGBUK: 'stu.cbe.go.kr',
+    CHUNGNAM: 'stu.cne.go.kr',
+    JEONBUK: 'stu.jbe.go.kr',
+    JEONNAM: 'stu.jne.go.kr',
+    GYEONGBUK: 'stu.gbe.go.kr',
+    GYEONGNAM: 'stu.gne.go.kr',
+    JEJU: 'stu.jje.go.kr'
+}
+
+SchoolAPI.Type = {
+    KINDERGARTEN: 1,
+    ELEMENTARY: 2,
+    MIDDLE: 3,
+    HIGH: 4
+}
+
+module.exports = SchoolAPI
